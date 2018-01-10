5827d49363279a582cd812feae626f5940916aa9
Michael Dalton <modalton@usc.edu>
2018-01-10 11:08:25 -0800

    fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,20 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {  
-
+function fizzbuzz(num){
+    let counter = 0;
+    let values = [];
+    while(num > counter){
+	counter++;
+	if( counter % 5 === 0 && counter % 3 === 0){
+	    values.push('fizzbuzz');
+	    continue;
+	}
+	if( counter % 3 === 0){ values.push('fizz') }
+	if( counter % 5 === 0){ values.push('buzz') }
+	if( counter % 5 !== 0 && counter % 3 !== 0){ values.push(counter)}
+    }
+    return values;
 }
 
 module.exports = fizzbuzz;
