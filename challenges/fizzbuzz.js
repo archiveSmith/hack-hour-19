46b9a835ec274ea3d06ce5122e938ec1e5165188
Louis Tang <ltsmooth@gmail.com>
2018-01-10 09:47:05 -0800

    completed fizzbuzz challenge

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,22 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {  
+function fizzbuzz(num) {
+  const array = [];
 
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push('fizzbuzz');
+    } else if (i % 3 === 0) {
+      array.push('fizz');
+    } else if (i % 5 === 0) {
+      array.push('buzz');
+    } else {
+      array.push(i);
+    }
+  }
+
+  return array;
 }
 
 module.exports = fizzbuzz;
