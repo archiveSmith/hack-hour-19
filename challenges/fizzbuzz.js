08f71d4b3a19bc9e7eb6e31e9d4820236e99ad9c
Eric Fileti <1425339+ericfileti@users.noreply.github.com>
2018-01-10 10:00:51 -0800

    fizzbuzzed

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,23 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {  
-
+function fizzbuzz(num) {
+    let result = [];
+    for(let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push('fizzbuzz');
+        }
+        else if (i % 3 === 0) {
+            result.push('buzz');
+        }
+        else if (i % 5 === 0) {
+            result.push('fizz');
+        }
+        else {
+            result.push(i);
+        }
+    }
+    return result;
 }
 
 module.exports = fizzbuzz;
