52c8edb0d291abf36e7644a7a4cc98c49e220402
Skyler Escobedo <skylerescobedo@gmail.com>
2018-01-13 09:38:47 -0800

    Solution to modeMean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
+    let len = array.length;
 
+    function mean(array){//the average number
+        let count = 0;
+        for (let i = 0; i < len; i++) count += array[i];
+        return count / len;
+    }
+
+    function mode(array){//how many
+        let table = {};
+        let answer = {num: null, count: 0};
+        for (let i = 0; i < array.length; i++) {
+            let num = array[i];
+            !table[num] ? table[num] = 1 : table[num] += 1;
+        }
+        for (const key in table) {
+            if(table[key] > answer['count']){
+                answer['count'] = table[key];
+                answer['num'] = key;
+            }
+        }
+        return answer.num;
+    }
+    return mode(array) == mean(array) ? true : false;
 }
-
 module.exports = modemean;
