4bf9f0c0d37c5a329c2893b34c611f13e566fa36
Michael Dalton <modalton@usc.edu>
2018-01-13 09:53:33 -0800

    mm

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,19 @@
 
 
 function modemean(array) {
+    let count = array.reduce((acc,cur) => {
+        if(!acc.hasOwnProperty(cur)){
+            acc[cur]=0;
+        }
+        acc[cur]++
+        return acc
+    },{})
+    let modeNum = Object.keys(count).reduce((acc,cur) => count[cur][0] > acc ? count[cur][0]:acc)
+    
+    let total = array.reduce((acc,cur) => acc+cur , 0)
+    let mean = Math.floor(total/array.length)
 
+    return total === mean
 }
 
 module.exports = modemean;
