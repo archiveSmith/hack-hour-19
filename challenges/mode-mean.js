e249b7370ee95d62c8e8db6cecd049bfb655de3c
Justin Yip <justinyip@Jeremys-MacBook-Air.local>
2018-01-13 09:45:40 -0800

    1/13 Finished meanmode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -8,10 +8,33 @@
  * Return true or false
  *
  */
-
-
 function modemean(array) {
-
-}
-
+    let mean = Math.floor(array.reduce((acc,ele) => acc + ele)/array.length);
+    let mode = checkMode(modeObj(array));
+    console.log(mean,mode)
+    return mode == mean;
+  }
+  
+  
+  function modeObj(array) {
+    var obj = {};
+    for (var i = 0; i < array.length; i++) {
+      if (!obj[array[i]]) {
+        obj[array[i]] = 1;
+      } else {
+        obj[array[i]]++;
+      }
+    }
+    return obj;
+  }
+  
+  function checkMode (obj) {
+    var largest = 0;
+    for (var i in obj) {
+      if (obj[i] >= largest) {
+        largest = i;
+      } 
+    }
+    return largest;
+  }
 module.exports = modemean;
