bfacdea79fc57264ba37b7657ec0387990fcccd0
Simon Park <simonpark107@gmail.com>
2018-01-13 10:05:19 -0800

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -8,10 +8,37 @@
  * Return true or false
  *
  */
-
+//mode is the most repeated number
+//mean is the average
 
 function modemean(array) {
+  let Array = array.sort().reverse();
+  let count = {};
+  let compare = 0;
+  let mode;
 
-}
+  let mean = array.reduce(function (sum, num) {
+    return sum + num;
+  }) / array.length
 
-module.exports = modemean;
+  for (var i = 0; i < Array.length; i++) {
+    if (count[Array[i]] === undefined) {
+      count[Array[i]] = 1;
+    } else {
+      count[Array[i]]++;
+    }
+
+    if (count[Array[i]] > compare) {
+      compare = count[Array[i]];
+      mode = Array[i];
+    }
+  }
+
+
+  if (Math.floor(mean) === mode) {
+    return true;
+  } else {
+    return false;;
+  }
+
+  module.exports = modemean;
