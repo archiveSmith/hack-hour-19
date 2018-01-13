46e932f16bb85f78605bc8a0e1d677e69460d429
Max White <maxwell.oliver.white@gmail.com>
2018-01-13 09:40:08 -0800

    meanmode complete, passed my tests for finding mean and mode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,37 @@
 
 
 function modemean(array) {
+  let arr = array.slice().sort();
+  let current;
+  let prev = array[0];
+  let count = 1;
+  let best = 1;
+  let mode = prev;
+  let total = prev;
 
+  for (let i = 1; i < arr.length; i ++) {
+    let current = arr[i];
+
+    // for finding mean
+    total += current;
+
+    // for finding mode
+    if (current === prev) {
+      count ++;
+    } else {
+      count = 1;
+    }
+    if (count >= best) {
+      mode = current;
+      best = count;
+    }
+    prev = current;
+  }
+
+  let mean = Math.floor(total / arr.length);
+  //console.log(mode);
+  //console.log(mean);
+  return (mean === mode);
 }
 
 module.exports = modemean;
