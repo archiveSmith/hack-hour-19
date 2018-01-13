0c827eb28f38c0b9252a4a380c31b38fba0c6c50
Eric Fileti <github.com@ericfileti.com>
2018-01-13 10:14:26 -0800

    no done yet

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,47 @@
  *
  */
 
+module.exports = modemean;
 
 function modemean(array) {
 
-}
+    let sum = 0;
+    let range = 0;
+    let median = 0;
+    let mean = 0;
+    let evenOrOddLength;
+    let min, max;
 
-module.exports = modemean;
+
+    array.forEach(function(el, i){
+        if (i === 0){
+            min = el;
+            max = el;
+        }
+        max = Math.max(max, el);
+        min = Math.min(min, el);
+        sum += el;
+    });
+    evenOrOddLength = array.length % 2;
+    // if (evenOrOddLength === 0){
+    //     // find the two middle numbers and average
+    //     median = array[evenOrOdd + 1] + array[evenOrOdd - 1];
+    // }
+    // else {
+    //     // goto the middle element and set median equal to it
+    //     median =
+    // }
+
+    mean = Math.floor(sum/array.length);
+    range = max - min;
+    console.log('array: ' + array.toString());
+    console.log('sum: ' + sum);
+    console.log('elements: ' + array.length);
+    console.log('mean: ' + mean);
+    console.log('max: ' + max);
+    console.log('min: ' + min);
+    console.log('range: ' + range);
+    console.log('evenOrOddLength: ' + evenOrOddLength);
+
+}
+modemean([1,3,5,7]);
