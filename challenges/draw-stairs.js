3befbc9e862c3bb04b1a32dae60ca5981d573439
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-01-13 10:01:58 -0800

    hack hour

---
+++ challenges/draw-stairs.js
@@ -0,0 +1,29 @@
+/* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
+ The staircase must climb up from left to right. The last line should only consist of asterisks,
+ without any leading/trailing spaces.
+ 
+ For example:     
+ drawStairs(6) ->          
+     *
+    **
+   ***
+  ****
+ *****
+******
+
+*/
+
+function drawStairs(n) {
+    let next;
+    let extra;
+    let stairs;
+    for (var i = 0; i < n; i++) {
+        next = n - 1 - i;
+        extra = i + 1;
+        stairs = ' '.repeat(next) + '*'.repeat(extra);
+        console.log(stairs);
+    }
+}
+
+
+module.exports = drawStairs;
---
+++ challenges/mode-mean.js
@@ -0,0 +1,34 @@
+/*
+ * Given an array of numbers, determine if the mode and mean of the array are equivalent
+ *
+ * Caveats:
+ * 	- Math.floor the mean
+ * 	- If there are multiple modes, use the max of the modes
+ *
+ * Return true or false
+ *
+ */
+
+
+function modemean(array) {
+    //calculate mean;
+    let totalMean = 0;
+    for (let i = 0; i < array.length; i++) {
+        totalMean += array[i];
+    }
+    totalMean = totalMean / array.length
+    console.log(totalMean)
+    //calculate mode;
+    let modeArr = {};
+    let arr = [];
+    for (let i = 0; i < array.length; i++) {
+        if (modeArr[array[i]] === undefined) {
+            modeArr[array[i]] = 0;
+        }
+
+        modeArr[array[i]] += 1
+    }
+    Object.values(modeArr);
+}
+
+module.exports = modemean;
