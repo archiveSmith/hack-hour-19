21c630b4e1de263dc5b781528f8bf4dda291bce9
Kunal Patel <kunalpatel73@gmail.com>
2018-01-13 10:00:19 -0800

    Completed.

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,23 @@
 
 
 function modemean(array) {
+    let meanSum = array.reduce((a, b) => a + b, 0);
+    let mean = Math.floor((meanSum / array.length)); 
+    let highestModeKey = 0;
+    
+    let modeObj = array.reduce(function(result, elem) {
+      result[elem] = (result[elem] || 0) + 1;
+      return result;
+    }, {});
+    
+    for (let key in modeObj) {
+      if (modeObj[key] > highestModeKey) {
+        highestModeKey = key;
+      }
+    }
 
+    return mean === Number(highestModeKey);
+    
 }
 
 module.exports = modemean;
