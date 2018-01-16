62ebd11b91bbf49db75ddfb7434087c87601509d
Louis Tang <ltsmooth@gmail.com>
2018-01-16 09:56:33 -0800

    added mode mean solution

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,42 @@
 
 
 function modemean(array) {
+  return mode(array) === mean(array);
+}
 
+function mode(array) {
+
+  const tally = {};
+
+  // get the counts
+  array.forEach(e => {
+
+    if (e in tally) {
+      tally[e] += 1;
+    } else {
+      tally[e] = 1;
+    }
+  });
+
+  // get the largest
+  let max = -Infinity;
+  let maxObject = {
+    value: -Infinity,
+    count: 1 };
+
+  Object.keys(tally).forEach(k => {
+    if (tally[k] >= maxObject.count) {
+      maxObject.max = k;
+      maxObject.count = tally[k];
+    }
+  });
+
+  return parseFloat(maxObject.max);
+}
+
+function mean(array) {
+  const sum = array.reduce((s, n) => (s + n), 0);
+  return Math.floor(sum / array.length);
 }
 
 module.exports = modemean;
