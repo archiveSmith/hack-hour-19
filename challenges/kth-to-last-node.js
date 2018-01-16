fd0cab8700f61a4a976adabaafd2283ccb85cce1
Kunal Patel <kunalpatel73@gmail.com>
2018-01-16 10:13:04 -0800

    attempted

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,32 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
+// do not know size of linked list 
+// use 2 pointers to initialize both to head of linked list
+// move 2nd pointer to nth node and keep 1st pointer at head
+// increment both 1st and 2nd pointers until 1st pointer.next not equal to null
+// 2nd pointer will be at nth from end of node.
 
+
+function kthToLastNode(k, head) {
+  if (!head || k < 1) {
+    return undefined;
+  }
+  var firstPointer = head;
+  var nthBehindPointer = head;
+  
+  for (var i = 0; i < k -1; i++) {
+    firstPointer = firstPointer.next;
+    if (!firstPointer) {
+      return undefined;
+    }
+  }
+  
+  while (typeof firstPointer.next !== 'undefined') {
+    nthBehindPointer = nthBehindPointer.next;
+    firstPointer = firstPointer.next;
+  }
+  return nthBehindPointer;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
