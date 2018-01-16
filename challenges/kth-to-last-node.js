5a0116539039559c26944e3be7c4909c63b8640d
Justin Yip <justinyip@Jeremys-MacBook-Air.local>
2018-01-16 10:02:42 -0800

    Finished kth to last

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,29 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  let count = 1;
+  let current = head;
+  while (current.next) {
+    current = current.next;
+    count++;
+  }
+  let newCurrent = head;
+  for (var i = 0; i < (count - k); i++) {
+    newCurrent = newCurrent.next;
+  }
+  return newCurrent.value;
 }
 
+  const a = new Node('A');
+  const b = new Node('B');
+  const c = new Node('C');
+  const d = new Node('D');
+  const e = new Node('E');
+ 
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  d.next = e;
+  
+kthToLastNode(2, a);  
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
