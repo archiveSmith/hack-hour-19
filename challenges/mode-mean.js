f370638cd8a12effb8b7251b69fc862b40b18e48
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-01-16 14:47:01 -0800

    mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,36 @@
  */
 
 
-function modemean(array) {
+ function modemean(array) {
+   let mean;
+   mean = Math.floor(array.reduce((a, b) => {return a+b;},0)/array.length);
+   return mean === calculateMode(array);
+ }
 
-}
+ function calculateMode(array) {
+   var numCount = {};
+   for (var i = 0; i < array.length; i++) {
+     let num = array[i];
+     if (!numCount[num]) {
+       numCount[num] = 1;
+     } else {
+       numCount[num]++;
+     }
+   }
+   let maxOccurrence = 0;
+   let maxValue = -Infinity;
+   for (var key in numCount) {
+     if (numCount[key] >= maxOccurrence) {
+       maxOccurrence = numCount[key];
+       if (Number(key) > maxValue) {
+         maxValue = Number(key);
+       }
+     }
+   }
+
+   return maxValue;
+ }
+
+
 
 module.exports = modemean;
