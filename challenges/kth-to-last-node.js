7b82beb1df8d46915579ee4b4bcf446c9f2537c9
Max White <maxwell.oliver.white@gmail.com>
2018-01-16 09:29:08 -0800

    seems to be working as expected, included tester function

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,39 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let next = head;
+  let nodeStack = [];
+  while (next) {
+    nodeStack.push(next);
+    next = next.next;
+  }
+  let kthNode = {
+    value: undefined
+  };
+  for (let kay = 0; kay < k; kay ++) {
+    kthNode = nodeStack.pop();
+  }
 
+  if (!kthNode) {
+    return undefined;
+  }
+
+  return kthNode.value;
 }
+
+function tests() {
+  let head = new Node(1);
+  let current = head;
+  for (let i = 2; i < 20; i ++) {
+    current.next = new Node(i);
+    current = current.next;
+  }
+
+  for (let i = 0; i < 21; i ++) {
+    console.log(kthToLastNode(i, head));
+  }
+}
+
+//tests();
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
