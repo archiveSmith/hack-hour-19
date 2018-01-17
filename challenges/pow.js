5e5354f166a96a047d37d261d4a5f7139a76af56
Tim <tjhong30@yahoo.com>
2018-01-17 09:36:59 -0800

    recurse

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,11 @@
  */
 
 function pow(base, power) {
+  let num =  base;
+  num * base;
+  if(power === 1) return num;
 
+  return num * pow(base, power-1);
 }
-
+console.log(pow(3, 3));
 module.exports = pow;
