1e1ec8b10b9ea8833fb1d8c69cbb141e82859724
Eric Fileti <github.com@ericfileti.com>
2018-01-17 09:58:35 -0800

    positive exponent power function

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,17 @@
  * Use recursion!
  */
 
+// will only calculate a positive power
 function pow(base, power) {
-
+    let sum = base;
+    power = (power < 0)? 0 : power;
+    if (power === 0) return 1;
+    function powFunc (base, power) {
+        if (power === 1) return sum;
+        sum = base * sum;
+        power--;
+        return powFunc (base, power);
+    }
+    return powFunc (base, power);
 }
-
 module.exports = pow;
