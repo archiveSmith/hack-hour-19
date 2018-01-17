6803dc74858f104c5616528f5dfd7aada02016dc
Monica <mshim89@gmail.com>
2018-01-17 09:48:41 -0800

    solution upload

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,16 @@
  */
 
 function pow(base, power) {
-
+if (power > 0) {
+    if (power<0) return;
+    if (power===0) return 1;
+    if (power>0) return base*pow(base,power-1);
+} else  {
+    if (power===0) return 1;
+    if (power<0) return (1/base)*pow(base,power+1);
+}
 }
 
 module.exports = pow;
+
+
