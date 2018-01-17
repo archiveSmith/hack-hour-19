0771d998780f5c95e3c456ea8d9b71e0115d0dd5
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-01-17 09:29:24 -0800

    pow handles positive and negative exponents

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,28 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
-}
+ function pow(base, power) {
+   function positive(b, p) {
+     if (p === 0) {
+       return 1;
+     } else if (p === 1) {
+       return b;
+     } else {
+       return b * positive(b, p-1);
+     }
+   }
+   function negative(b, p) {
+     if (p === -1) {
+       return 1/b;
+     } else {
+       return 1/b * negative(b, p+1);
+     }
+   }
+   if (power < 0) {
+     return negative(base, power);
+   } else {
+     return positive(base, power);
+   }
+ }
 
 module.exports = pow;
