5b362126ea5e603b657af1893ebd4cc891e301af
Louis Tang <ltsmooth@gmail.com>
2018-01-19 09:34:39 -0800

    solved reverse challenge

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,13 @@
  */
 
 function reverseInPlace(array) {
+  for (let i = 0; i < array.length / 2; i += 1) {
+    const temp = array[i];
+    array[i] = array[array.length - i - 1];
+    array[array.length - i - 1] = temp;
+  }
 
+  return array;
 }
 
 module.exports = reverseInPlace;
