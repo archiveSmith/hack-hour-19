e8f4dfb1c13c3a57b645cec01349ed6045ff6b6c
Max White <maxwell.oliver.white@gmail.com>
2018-01-20 09:31:33 -0800

    binary to decimal tested, seems to work

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,38 @@
  */
 
 function binToDec(binary) {
+  let mult = 1;
+  let dec = 0;
+  for (let i = binary.length-1; i >=0; i--) {
+    let digit = parseInt(binary.charAt(i))
+    dec += digit * mult;
+    mult *= 2;
+  }
+  return dec;
+}
+
+function decToBin(binary) {
 
 }
 
-module.exports = binToDec;
+function decToHex(binary) {
+
+}
+
+function tests() {
+  console.log(binToDec('1001'));
+  console.log(binToDec('0'));
+  console.log(binToDec('11'));
+  console.log(binToDec('100'));
+  console.log(binToDec('101'));
+  console.log(binToDec('0101'));
+  /*
+  binToDec('0')   -> 0
+   * 	binToDec('11')  -> 3
+   * 	binToDec('100') -> 4
+   * 	binToDec('101') -> 5
+   *  binToDec('0101') -> 5
+   */
+}
+tests()
+//module.exports = binToDec;
