f4cce6c6ad3205f681307960568be557f3ca0d53
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-01-23 10:07:04 -0800

    Balanced Parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,35 @@
  */
 
 function balancedParens(input){
+  const parens = [];     // ()
+  const brackets = [];   // []
+  const curly = [];      // {}
+  let last = '';
 
+  if (!input) return false;
+
+  const inputArr = input.split('');
+
+  for (var i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] === '(') {
+        parens.push(inputArr[i]);
+    } else if (inputArr[i] === ')') {
+        if (parens.length === 0) return false;
+        else parens.pop();
+    } else if (inputArr[i] === '[') {
+        brackets.push(inputArr[i]);
+    } else if (inputArr[i] === ']') {
+        if (brackets.length === 0) return false;
+        else brackets.pop();
+    } else if (inputArr[i] === '{') {
+        curly.push(inputArr[i]);
+    } else if (inputArr[i] === '}') {
+        if (curly.length === 0) return false;
+        else curly.pop();
+    }
+
+  }
+  return parens.length === 0 && brackets.length === 0 && curly.length === 0;
 }
 
 module.exports = balancedParens;
