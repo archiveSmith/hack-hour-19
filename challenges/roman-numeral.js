5ba02810224ebd20c3c8710418880e1f7a3734fe
Kunal Patel <kunalpatel73@gmail.com>
2018-01-24 10:02:47 -0800

    completed

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,17 @@
  */
 
 function romanNumeral(n) {
-
+    var container = '';
+    var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var romans = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+      
+    for (var i = 0;i<=decimal.length;i++) {
+      while (n%decimal[i] < n) {   
+        container += romans[i];
+        n -= decimal[i];
+      }
+    }
+    return container;
 }
 
 module.exports = romanNumeral;
