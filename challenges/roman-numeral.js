fe0d674d643f1a8fd3509fdeeda2fa17752e11d2
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-01-24 10:01:18 -0800

    roman numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,26 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
-function romanNumeral(n) {
+ function romanNumeral(n) {
+   let roman = ""; // Will be string representation of the roman numeral
+   let number = n;
+   const chart = [ ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90],
+   ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
 
-}
+
+
+   if (n === 0) return "";
+   else {
+     let index = chart.findIndex((element) => element[1] <= number);
+     roman = chart[index][0];
+     number -= chart[index][1];
+
+     return roman.concat(romanNumeral(number));
+   }
+ }
+
 
 module.exports = romanNumeral;
