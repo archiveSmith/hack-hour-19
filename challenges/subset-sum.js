b31fed3e5db19915cd437d07c63d553e2c79de1c
Max White <maxwell.oliver.white@gmail.com>
2018-01-26 09:55:24 -0800

    doesn't work for negative numbers

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,7 +1,7 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
  * there is a subset of the array that sums up to the target and returns false otherwise. A subset
  * can be any size and the elements do not have to appear consecutively in the array.
- * 
+ *
  * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
@@ -9,7 +9,31 @@
  */
 
 function subsetSum(array, target) {
+  let nums = array.slice();
+  nums = nums.sort();
+  let sums = new Array(target+1);
+  sums.fill(false);
+  sums[0] = true;
 
+  for (num of nums) {
+    for (let i = sums.length-1; i >= 0; i--) {
+      if (sums[i]) {
+        sums[i+num] = true;
+      }
+    }
+  }
+
+
+  return sums[target];
 }
+
+function tests() {
+  console.log(subsetSum([3, 7, 4, 2], 5)) // true, 3 + 2 = 5
+  console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)) // true, 3 + 12 + 5 + 12 = 32
+  console.log(subsetSum([8, 2, 4, 12], 13)) // false
+  console.log(subsetSum([8, -2, 1, -3], 6)) // true, 8 + 1 + (-3) = 6
+}
+
+//tests();
 
 module.exports = subsetSum;
