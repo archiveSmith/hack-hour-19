27bc4b260edd4e54b4d04395447bb9011b47851b
Xavyr Moss <xmoss10@gmail.com>
2018-01-30 10:08:02 -0800

    match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -23,30 +23,6 @@ function matchWord(str) {
 				return false;
 			}
 		}
-	} else if(str.includes('IF')) {
-		//let first = str.split('IF')[0];
-		let second = str.split('IF')[1];
-		if(second.includes('IF')) {
-			matchWord(second);
-		} else {
-			if(second.slice(1).includes('fi')) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	} else if(str.includes('FOR')) {
-		//let first = str.split('FOR')[0];
-		let second = str.split('FOR')[1];
-		if(second.includes('FOR')) {
-			return matchWord(second);
-		} else {
-			if(second.slice(1).includes('ROF')) {
-				return true;
-			} else {
-				return false;
-			}
-		}
 	} else if(str.includes('while')) {
 		//let first = str.split('while')[0];
 		let second = str.split('while')[1];
@@ -59,17 +35,41 @@ function matchWord(str) {
 				return false;
 			}
 		}
+	} else if(str.includes('IF')) {
+		//let first = str.split('IF')[0];
+		let second = str.split('IF')[1];
+		if(second.includes('IF')) {
+			matchWord(second);
+		} else {
+			if(second.slice(1).includes('fi')) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	} else if(str.includes('for')) {
+		//let first = str.split('FOR')[0];
+		let second = str.split('for')[1];
+		if(second.includes('for') && second.includes('if')) {
+			return matchWord(second);
+		} else {
+			if(second.slice(1).includes('for') && second.slice(1).includes('if')) {
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 	return true;
 }
 
+//
+// console.log(matchWord('__END_DNE-----')); // -> true
+// console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+// console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ]
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')); // -> true
+// console.log(matchWord('')); // -> true
 
-console.log(matchWord('__END_DNE-----')); // -> true
-console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
-console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
-console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ]
-console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw')); // -> true
-console.log(matchWord('')); // -> true
-
-//module.exports = matchWord;
+module.exports = matchWord;
