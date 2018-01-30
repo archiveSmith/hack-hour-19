2da039a2b1dadff8242e9c879709af660ba758a2
Kunal Patel <kunalpatel73@gmail.com>
2018-01-30 10:06:50 -0800

    almost there

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,23 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+    strFilter = str.replace(/[\W_]+/g," ").trim();
+    strFilter = strFilter.toLowerCase().split(' ');
+    if(strFilter.length % 2 !== 0) return false;
+    
+    let result = [];
+    for (let i = 0; i < strFilter.length; i += 1) {
+      result.push(strFilter[i].split('').sort());
+    }
+    
+    for (let j = 0; j < result.length; j += 1) {
+      if (result[j].includes(result[j + 1])) {
+        return true;
+      }
+      return false;
+    }
+    
+    //not done yet, but almost there!!
 }
 
 module.exports = matchWord;
