17f2ad2d5d685fe6524b16a617f9a393f327c442
Louis Tang <ltsmooth@gmail.com>
2018-01-30 10:01:48 -0800

    match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,65 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  const stack = [];
+  // const letters = str.match(/[a-zA-Z]/g);
+
+
+  // if (str.split('_').length % 2 !== 0) {
+  //   return false;
+  // }
+  const array = str.split(/[\s,_]+/);
+
+  const clean = array.map(element => {
+    return element.replace(/[^\w]/g, '').toLowerCase();
+  });
+
+
+
+  const wordMap = { while: 'elihw', 
+                    try: 'yrt',
+                    for: 'rof',
+                    if: 'fi',
+                    end: 'dne'};
+
+  const noEmpties = [];
+
+  clean.forEach(item => {
+    if (item.length > 0) {
+      noEmpties.push(item);
+    }
+  })
+  
+  // if (noEmpties.length === 1) {
+    // return false;
+  //   if (clean.join('') === clean.reverse().join('')) {
+  //     return true;
+  //   }
+  // }
+
+  let good = true;
+  noEmpties.forEach(item => {
+    if (Object.values(wordMap).includes(item)){
+      const savedWord = stack.pop();
+      if (savedWord.split('').reverse().join('') !== item) {
+        good = false
+      } 
+    } else {
+      stack.push(item);
+    }
+  });
+
+  return good;
+// if fi, case esac, while elihw
+// words separated by space or punctuation
 
 }
+
+console.log(matchWord('__END_DNE-----'));
+console.log(matchWord('__ENDDNE__'));
+console.log(matchWord('IF()()fi[]'));
+console.log(matchWord('for__if__rof__fi'));
+console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));
+console.log(matchWord(''));
 
 module.exports = matchWord;
