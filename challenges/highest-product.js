d58c2673edfa342122c63fb265ec08a6b4e6de0e
Kunal Patel <kunalpatel73@gmail.com>
2018-01-31 09:44:29 -0800

    solved errors

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,7 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3 || !Array.isArray(array)) { return 0; }
   let arraySort = array.sort((a,b) => {return b-a});
   let answer = arraySort[0] * arraySort[1] * arraySort[2];
   return answer;
