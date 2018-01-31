b0cab3c7df2875cff832be8d9963f4dee9dd727b
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-01-31 10:52:58 -0800

    highest-product.js

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,12 +3,16 @@
  */
 
 function highestProduct(array) {
-  if (array.length < 3 || !Array.isArray(array)) {
+  if (array.length < 3 || !Array.isArray(array) || (array.length === 3 && array.indexOf(0) > 0)) {
     return 0;
-  } else {
-    var arr = array.map(Math.abs).sort();
-    return arr.slice(-3).reduce((a,b) => a*b,1);
   }
+  let negs = array.filter(x => x<0);
+  let largestNeg = negs.map(Math.abs).sort();
+  let largeNeg = largestNeg.slice(-2).reduce((a,b) => a*b, 1);
+  let pos = array.filter(x => x >= 0).sort();
+  let largePos = pos.slice(-2).reduce((a,b) => a*b, 1);
+  return largeNeg * pos[pos.length-1];
+
 }
 
 
