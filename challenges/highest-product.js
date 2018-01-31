e48beb104d044190619600fd7d0c07f99805e767
Chris Jeon <chrisjwas@gmail.com>
2018-01-31 09:35:50 -0800

    finish challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,26 @@
  */
 
 function highestProduct(array) {
+  // Check if array has sufficient length
+  if (array.length < 3) {
+    return 'Array is too small. Need more numbers';
+  }
+  // Initialize 'product' and 'count' variables
+  let product = 1;
+  let count = 0;
 
+  // Sort array
+  const sortArr = array.sort((a, b) => {
+    return a - b;
+  });
+
+  // Get the product of the 3 largest numbers in array
+  for (let i = sortArr.length - 1; i > 0 && count < 3; i -= 1) {
+    product *= sortArr[i];
+    count += 1;
+  }
+
+  return product;
 }
 
 
