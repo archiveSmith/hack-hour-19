b30d32c0ac331a2f5e08af0d4a511a2a9928578c
Zihao <zi5253hao@gmail.com>
2018-01-31 10:01:16 -0800

    Done

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,38 @@
  */
 
 function highestProduct(array) {
-
+	// case for less than array.length <3
+	if(array.length<=3){
+		let product = 1; 
+		for(let el of array){
+			product*=el; 
+		}
+		return product; 
+	}
+	// else
+	let maxProduct = 1; 
+	for(let i=0; i<array.length; i++){
+		let currentNum = array[i]; 
+		let withoutNumArr = array.splice(i, 1); 
+		for(let j=0; j<withoutNumArr.length; j++){
+			let currentSet
+			if(j==withoutNumArr.length-1){
+				currentSet = currentNum.concat(withoutNumArr.slice(j), withoutNumArr.slice(0,1));  
+			}
+			else{
+				currentSet = currentNum.concat(withoutNumArr.slice(j, j+2));
+			}
+			let currentProduct = 1; 
+			for(let el of currentSet){
+				currentProduct*=el; 
+			}
+			if(currentProduct>maxProduct){
+				maxProduct = currentProduct;
+			}
+			currentProduct = 1; 
+		}
+	}
+	return maxProduct; 
 }
 
 
