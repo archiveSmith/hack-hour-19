38a8a2268528cd3d4e7dfef561440fa77d915ab3
Justin Yip <justinyip@Jeremys-MacBook-Air.local>
2018-01-31 09:58:40 -0800

    Fixed an Error

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,7 @@
  */
 
 function highestProduct(array) {
+    if (array.length === 0 || !Array.isArray(array)) {return 0};
     let arr = array.sort((a,b) => b-a);
     let productNegative = arr[0] * arr[arr.length-1] * arr[arr.length-2];
     let productPositive = arr.slice(0,3).reduce((acc, ele) => acc * ele);
