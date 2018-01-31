484e983d3d7c0cd03f1e72af1094f1f0eb4c0ffc
saltyandsmiling <eric.rudolph.carrillo@gmail.com>
2018-01-31 09:53:01 -0800

    second try

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,8 +2,15 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+function sortNumber(a,b) {
+  return a - b;
+}
 
+function highestProduct(array) {
+  if (!Array.isArray(array) || array.length < 3) return 0;
+  const sortedArr = array.sort(sortNumber).slice(-4);
+  sortedArr[0] * sortedArr[2] < 0 && sortedArr[1] * sortedArr[3] < 0 ? sortedArr.pop() : sortedArr.shift();
+  return sortedArr.reduce((a, b) => a*b);
 }
 
 
