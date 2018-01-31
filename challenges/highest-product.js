3d2cae9d70c0b1b575601261af9c0b48c4e23b35
Simon Park <simonpark107@gmail.com>
2018-01-31 09:54:54 -0800

    highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -20,6 +20,6 @@ function highestProduct(array) {
   // });
 }
 
-console.log(highestProduct([-10, 6, 5, 4, 3, 2, 1]));
+// console.log(highestProduct([-10, 6, 5, 4, 3, 2, 1]));
 
 module.exports = highestProduct;
