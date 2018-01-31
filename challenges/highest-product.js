3d8d0d99e4f340694a8ce1c20aff2b3f69dc8c5b
Xavyr Moss <xmoss10@gmail.com>
2018-01-31 09:37:52 -0800

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,12 @@
  */
 
 function highestProduct(array) {
-
+	let sorted = array.sort(function (a, b){
+		return b > a;
+	});
+	return sorted[0] * sorted[1] * sorted[2];
 }
 
+//console.log(highestProduct([100, 10, 3, 2, 5, 6]));
 
 module.exports = highestProduct;
