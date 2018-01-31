97b5b85c84457592d19e83f4e2ac0b4383aac8e6
Xavyr Moss <xmoss10@gmail.com>
2018-01-31 10:02:07 -0800

    highest product post

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,12 +3,42 @@
  */
 
 function highestProduct(array) {
-	let sorted = array.sort(function (a, b){
+	console.log('array', array);
+	let largestThree = [];
+
+
+	let mapped = array.map(function (el){
+		if(el > 0) {
+			return [el, 'pos'];
+		} else {
+			return [el * -1, 'neg'];
+		}
+	});
+
+	let sorted = mapped.sort(function (a, b){
 		return b > a;
 	});
-	return sorted[0] * sorted[1] * sorted[2];
+
+	let count = 0;
+	for(let i = 0; i < 3; i++) {
+		if(sorted[i][1] === 'neg'){
+			count++;
+		}
+	}
+	if(count % 2 === 0) {
+		return sorted[0][0] * sorted[1][0] * sorted[2][0];
+	} else {
+		largestThree = [];
+		for(let i = 0; i < sorted.length; i++) {
+			if(sorted[i][1] === 'pos' && largestThree.length <= 3) {
+				largestThree.push(sorted[i][0]);
+			}
+		}
+		return largestThree[0] * largestThree[1] * largestThree[2];
+	}
+
 }
 
-//console.log(highestProduct([100, 10, 3, 2, 5, 6]));
+console.log(highestProduct([2, -2, -3, -6]));
 
-module.exports = highestProduct;
+//module.exports = highestProduct;
