269422c6676639ec9611afeba807f8f7ecc30a92
Tim <tjhong30@github.com>
2018-01-31 09:28:49 -0800

    product only

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -6,6 +6,6 @@ function highestProduct(array) {
     let sorted = array.sort().reverse();
     return sorted[0] * sorted[1] * sorted[2];
 }
-// console.log(highestProduct([2,3,6,1]));
+// console.log(highestProduct([2,3,6,1]));``
 
 module.exports = highestProduct;
