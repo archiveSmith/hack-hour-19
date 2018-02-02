bfc44acb06e7bc289406285c925bf0a4067592bb
Louis Tang <ltsmooth@gmail.com>
2018-02-02 09:59:07 -0800

    removed checks

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -43,20 +43,6 @@ function zip(l1, l2) {
   return nodeHead;
 }
 
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-const node5 = new Node(5);
-const node6 = new Node(6);
 
-node1.next = node3;
-node3.next = node5;
-
-node2.next = node4;
-node4.next = node6;
-
-
-console.log(zip(node1, node2));
 
 module.exports = {Node: Node, zip: zip};
