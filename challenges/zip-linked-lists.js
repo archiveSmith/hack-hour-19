64fa79813b0798bd2fe93e954566c490740240ad
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-02-02 10:00:40 -0800

    linked list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,36 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+	let l1Copy = l1;
+	let l2Copy = l2;
+	let head = l1;
+	let pointer = "l2";
+	
+	if(!l1) return l2;
+	if(!l2) return l1;
+	
+	while(l1Copy && l2Copy) {
+		if(pointer === "l2") {
+			if(!l1Copy.next) break;
+			l1Copy = l1Copy.next;
+			head.next = head = l2Copy;
+			pointer = "l1";
+		} else {
+			if(!l2Copy.next) break;
+			l2Copy = l2Copy.next;
+			head.next = head = l1Copy;
+			pointer = "l2";
+		}
+	}
+	
+	if(!l1Copy.next) {
+		head.next = l2Copy;
+	} else {
+		head.next = l1Copy;
+	}
+	
+	return l1;
+	
+}
 
 module.exports = {Node: Node, zip: zip};
