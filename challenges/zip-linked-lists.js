fbb2cc7a3656efaf9e30b154d58000bf2bfe6441
Zihao <zi5253hao@gmail.com>
2018-02-02 10:01:26 -0800

    Need to work on it more

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,33 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+	// base case if one of the linkied is empty, retur non empty 
+
+	//while both linked list has value  
+	let current1 = l1.head; 
+	let current2 = l2.head; 
+	let zipList = new Node(current.value);
+	current1 = current1.next;
+	current2sTurn = true; 
+	while(current1 && current12){	
+		if(current2sTurn){
+			zipList.next = current2.value; 
+			current2 = current2.next; 
+			current2sTurn = false; 
+		}
+		else{
+			zipList.next = current1.value; 
+			current1 = current.next; 
+			current2sTurn = true; 
+		}
+		if(current2sTurn){
+			zipList.next = current2; 
+		}
+		else{
+			zipList.next = current1; 
+		}
+		return zipList; 
+	}
 };
 
 module.exports = {Node: Node, zip: zip};
