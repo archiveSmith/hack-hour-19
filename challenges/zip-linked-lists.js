09cb886623bbfbbc6a2549b946db04f619aff792
Louis Tang <ltsmooth@gmail.com>
2018-02-02 09:25:49 -0800

    zip linked lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,16 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  let currentNode1 = l1;
+  let currentNode2 = l2;
+
+  while (currentNode1 !== null && currentNode2 !== null) {
+    const tempNode = currentNode1.next;
+    currentNode1.next = currentNode2.next;
+    currentNode2.next = tempNode;
+    currentNode1 = currentNode1.next;
+    currentNode2 = currentNode2.next;
+  }
+}
 
 module.exports = {Node: Node, zip: zip};
