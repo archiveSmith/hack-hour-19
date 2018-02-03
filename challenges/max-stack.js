1d49795b82ef84a0583a2b89a6061f80c13a9cb6
Paul Dubovsky <pauldubovsky@Pauls-MacBook-Air.local>
2018-02-03 09:27:05 -0800

    max-stack.js

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.length = 0;
+  this.storage = {};
+}
+
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  return this.count;
+}
+
+Stack.prototype.pop = function() {
+  if (this.count === 0) {
+    return undefined;
+  }
+  this.count--;
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
+}
+
+Stack.prototype.getMax = function() {
+  var values = Object.values(this.storage);
+  var largest = -Infinity;
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] > largest) {
+      largest = values[i];
+    }
+  }
+  return largest;
 }
 
 module.exports = Stack;
