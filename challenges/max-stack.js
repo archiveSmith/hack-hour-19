5ebc9f99a26bbef57752c2fac183b2990e642b0f
Starvon Washington <staronejazz@yahoo.com>
2018-02-03 09:43:50 -0800

    getMax

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,30 @@
  */
 
 function Stack() {
-  // body...
+  let items = [];
+
+  this.push = (element) => {
+    items.push(element);
+    return items.length;
+  }
+
+  this.pop = () => {
+    return items.pop();
+  }
+
+  this.getMax = () => {
+    return Math.max(...items);
+  }
+
 }
 
 module.exports = Stack;
+
+let stack = new Stack();
+console.log(stack.push(3));
+console.log(stack.push(4));
+console.log(stack.pop(4));
+console.log(stack.push(26));
+console.log(stack.push(87));
+console.log(stack.push(8));
+console.log(stack.getMax());
