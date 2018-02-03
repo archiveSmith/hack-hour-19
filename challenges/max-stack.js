86e3b5cd85deba39edb6ecfa0ac1a5029742293b
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-02-03 09:54:45 -0800

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.values = [];
+  this.maxVal = 0;
 }
+
+Stack.prototype.push = function (value) {
+  if(value > this.maxVal) this.maxVal = value;
+  this.values.push(value);
+  return this.values.length;
+}
+
+Stack.prototype.pop = function () {
+  const popped = this.values.pop();
+  const length = this.values.length;
+
+  //Checking to see if the popped value is the maxVal;
+  if (popped === this.maxVal) {
+    this.maxVal = 0;
+    for (let index = 0; index < length; index++) {
+      if (this.values[index] > this.maxVal) this.maxVal = this.values[index];
+    }
+  }
+}
+
+Stack.prototype.getMax = () => {
+  return this.maxVal;
+}
+
+
+
+
 
 module.exports = Stack;
