1496a01332d35818d3a354f71d4e9e68712534ad
Michael Dalton <modalton@usc.edu>
2018-02-07 09:35:20 -0800

    hh

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,14 @@
  */
 
 function mergeArrays(arr1, arr2) {
+  if(arr1.length === 0){ return arr2 }
+  if(arr2.length === 0){ return arr1 }
 
+  if(arr1[0] < arr2[0]){
+    return [arr1.shift()].concat(mergeArrays(arr1,arr2))
+  }
+  return [arr2.shift()].concat(mergeArrays(arr1,arr2))
 }
+
 
 module.exports = mergeArrays;
