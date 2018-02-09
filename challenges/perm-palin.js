7c06e855c144236e00351a58cc2980aa53f7e2e8
Kunal Patel <kunalpatel73@gmail.com>
2018-02-09 09:45:45 -0800

    completed

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,32 @@
  */
 
 function permPalin(str) {
-	
+    let strForward = str.split('');
+    let strObjToArr = [];
+  
+    let strObj = strForward.reduce((result, letter) => {
+      result[letter] = (result[letter] || 0) + 1;
+      return result;
+    }, {});
+  
+    for (let key in strObj) {
+      strObjToArr.push(strObj[key]);
+    }
+    
+    strObjToArr.sort((a,b) => {return b-a});
+  
+    if (strObjToArr.length === 1) {
+      return true;
+    }
+    
+    else { 
+      for (let i = 0; i < strObjToArr.length; i += 1) {
+        if (strObjToArr[i] === strObjToArr[i + 1]) {
+          return true;
+        }
+        return false;
+      }
+    }
 }
 
 module.exports = permPalin;
