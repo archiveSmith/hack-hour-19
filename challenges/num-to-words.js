7e98daf24abc5a2ec90ba1fb726e0407f8489dd0
Chris Jeon <chrisjwas@gmail.com>
2018-02-10 09:43:07 -0800

    challenge

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,18 @@
  */
 
 function numToWords(num) {
-
+ let obj = {
+   1: 'one',
+   2: 'two',
+   3: 'three',
+   4: 'four',
+   5: 'five',
+   6: 'six',
+   7: 'seven',
+   8: 'eight',
+   9: 'nine',
+   10: 'ten'
+ }
 }
 
 module.exports = numToWords;
