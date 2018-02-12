afbdf484437741cdcddefe52a6a9d8275e435b50
Simon Park <simonpark107@gmail.com>
2018-02-12 09:33:08 -0800

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -2,19 +2,44 @@
  * Create a stack.Then create a queue using two stacks.
  */
 
-
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
 }
 
+Stack.prototype.push = function (value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Stack.prototype.pop = function () {
+  const result = this.storage[this.index - 1];
+  delete this.storage[this.index - 1];
+  this.index -= 1;
+  return result;
+};
 
 /**
-* Queue Class
-*/
-
+ * Queue Class
+ */
 
 function Queue() {
-
+  this.storage = {};
+  this.index = 0;
+  this.dequeueCount = 0;
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function () {
+  if (Object.keys(this.storage).length === 0) return undefined;
+  const dequeueItem = this.storage[this.dequeueCount];
+  delete this.storage[this.dequeueCount];
+  this.dequeueCount++;
+  return dequeueItem;
+};
+
+module.exports = { Stack, Queue };
