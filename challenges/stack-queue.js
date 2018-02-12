da82473a1feccc607bb5257dc1bef3ea6500a802
Kunal Patel <kunalpatel73@gmail.com>
2018-02-12 10:01:06 -0800

    attempted

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,15 @@
 
 
 function Stack() {
-
+  this.stack = [];
+  
+  this.pop = function() {
+    return this.stack.pop();
+  };
+  
+  this.push = function(item) {
+    return this.stack.push(item);
+  };
 }
 
 
@@ -14,7 +22,24 @@ function Stack() {
 
 
 function Queue() {
+  this.inbox = [];
+  this.outbox = [];
+};
 
-}
+Queue.prototype.push = function(val) {
+  this.inbox.push(val);
+  };
+  
+Queue.prototype.pop = function() {
+  if (!this.outbox.length) {
+    if (!this.inbox.length) {
+      return undefined;
+    }
+    while (this.inbox.length) {
+      this.outbox.push(this.inbox.pop());
+    }
+  }
+  return this.outbox.pop();
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
