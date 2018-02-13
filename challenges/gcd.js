99e0455d5c2ab57e101560decc6de1a849a9f42e
Zihao <zi5253hao@gmail.com>
2018-02-13 09:22:01 -0800

    version 1

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,20 @@
  */
 
 function gcd(a, b) {
-
+	let smaller;
+	if(a<b){
+		smaller = a;
+	}
+	else{
+		smaller = b; 
+	}
+	while(smaller>0){
+		if((a%smaller===0) && (b%smaller===)){
+			return smaller; 
+		}
+		smaller--; 
+	}
+	return 0;
 }
 
 module.exports = gcd;
