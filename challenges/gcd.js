8dabcee0841e7f8ceeb9ccfd6b2989cca5094b31
Andrew <afuselier23@gmail.com>
2018-02-13 09:25:51 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,14 @@
  */
 
 function gcd(a, b) {
-
+  let div = b;
+  if (b > a) div = a;
+  while (div > 0) {
+    if (a % div === 0 && b % div === 0) return div;
+    if (a === div || b === div) div = Math.floor(div / 2);
+    else div -= 1;
+  }
+  return 1;
 }
 
 module.exports = gcd;
