b45d8b1b49b6988dac5f0031af61a5aee7336040
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-02-13 09:25:27 -0800

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,6 +9,21 @@
 
 function gcd(a, b) {
 
+    if (a%b === 0) return b;
+    if (b%a === 0) return a;
+
+
+    let gd = 0;
+    let length = (a < b) ? a : b;
+    length = Math.ceil(Math.sqrt(length));
+
+    for (let i = 1; i <= length; i++) {
+        if (a%i === 0 && b%i === 0) {
+            gd = i;
+        }
+    }
+
+    return gd;
 }
 
 module.exports = gcd;
