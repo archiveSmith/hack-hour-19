9f4f4798f23d7f15f451582124ac77b749695342
Monica <mshim89@gmail.com>
2018-02-13 09:22:26 -0800

    solution upload

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,6 +9,15 @@
 
 function gcd(a, b) {
 
+let min = Math.min(a,b);
+let gcd;
+
+for (let i=1; i<=min; i++) {
+    if (a%i === 0 && b%i === 0) {
+        gcd = i;
+    }
+}
+return gcd;
 }
 
 module.exports = gcd;
