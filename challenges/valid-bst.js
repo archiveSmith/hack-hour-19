caa41269ce348dfbb3b5664003835a8e64822149
Mejin Leechor <amleechor@gmail.com>
2018-02-14 09:33:42 -0800

    Valid bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -7,13 +7,42 @@
  
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
-
+  if (!tree) return false;
+  // console.log('Tree value:', tree.value);
+  let leftValid = true, rightValid = true;
+  if (tree.left) {
+    if (tree.left.value > tree.value) {
+      // console.log('Tree left value:', tree.left.value);
+      // console.log('Invalid left value - returning false');
+      return false;
+    }
+    // console.log('Checking left branch');
+    leftValid = validBST(tree.left);
+  }
+  if (tree.right) {
+    if (tree.right.value < tree.value) {
+      // console.log('Tree right value:', tree.right.value);
+      // console.log('Invalid right value - returning false');
+      return false;
+    }
+    // console.log('Checking right branch');
+    rightValid = validBST(tree.right); 
+  }
+  return leftValid && rightValid;
 }
+
+// const bst = new BinaryTree(3);
+// bst.left = new BinaryTree(2);
+// bst.right = new BinaryTree(5);
+// bst.right.left = new BinaryTree(8);
+// bst.right.right = new BinaryTree(7);
+// console.log(bst);
+// console.log(validBST(bst));
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
