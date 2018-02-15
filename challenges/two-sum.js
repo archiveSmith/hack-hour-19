5688d39c1e55311657319597eba40e3334f75805
Xavyr Moss <xmoss10@gmail.com>
2018-02-15 09:52:45 -0800

    two sum

---
+++ challenges/two-sum.js
@@ -0,0 +1,20 @@
+/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+ *  array that sum up to the target value; return false otherwise
+ */
+
+function twoSum(arr, n) {
+	let yes = false;
+	while (arr.length > 0 && (!yes)) {
+		let popped = arr.pop();
+		console.log(arr, popped);
+		arr.forEach(function (el) {
+			if (el + popped === n) {
+				yes = true;
+			}
+		})
+	}
+	return yes;
+}
+
+//console.log(twoSum([10, -8, 6, 4, -2, 1], 11 ));
+module.exports = twoSum;
