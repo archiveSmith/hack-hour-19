ecf5c5e2e0fdd9129e4d5602860d2f21a16d8f91
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-02-15 09:16:34 -0800

    changes?

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -36,3 +36,20 @@ function permPalin(str) {
 
 
 module.exports = permPalin;
+
+
+// ========= Solution ============== /
+// function permPalin(str) {
+//   const obj = {};
+//   let numOdds = 0;
+
+//   for (let i = 0, ilen = str.length; i < ilen; i += 1) {
+//     obj[str[i]] = (obj[str[i]] += 1) || 1;
+//   }
+
+//   for (let value of Object.values(obj)) {
+//     numOdds += value % 2;
+//     if (numOdds > 1) return false;
+//   }
+//   return true;
+// }
