08a030d8b1bcd3cae41e214de8b18faf9e871a53
Michael Dalton <modalton@usc.edu>
2018-02-15 09:52:37 -0800

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,19 @@
  */
 
 function twoSum(arr, n) {
+  let array = arr.slice();
+  if (array.length === 1) {
+    return array[0] === n;
+  }
 
+  const num = array.shift();
+  const flag = array.reduce((acc, cur) => {
+    return cur + num === n ? true : acc;
+  }, false);
+
+  if (flag) return flag;
+
+  return twoSum(array, n);
 }
 
 module.exports = twoSum;
