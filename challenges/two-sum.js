aa9c1abb0d6f71beac4b965cf37ae005f028bf88
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-02-15 09:25:31 -0800

    twoSum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
+    const hashTable = {};
+    for (let i = 0; i < arr.length; i++) {
+        const diff = n - arr[i];
+        if (hashTable[diff]) return true;
+        else hashTable[arr[i]] = arr[i];
+    }
 
+    return false;
 }
 
 module.exports = twoSum;
