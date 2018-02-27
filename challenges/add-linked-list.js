77f8f431d64a1da2ba6d9f011fb41693da793e1c
Simon Park <simonpark107@gmail.com>
2018-02-27 09:51:15 -0800

    add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,29 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let head1 = l1;
+  let head2 = l2;
 
+  while (head1 && head2) {
+    head1.value += head2.value;
+    if (head1.value > 9) {
+      head1.value -= 10;
+      head1.next.value += 1;
+    }
+    head1 = head1.next;
+    head2 = head2.next;
+  }
+  return l1;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+const list1 = new Node(2);
+list1.next = new Node(1);
+list1.next.next = new Node(5);
+
+const list2 = new Node(5);
+list2.next = new Node(9);
+list2.next.next = new Node(2);
+
+console.log(addLinkedList(list1, list2));
+
+module.exports = { Node, addLinkedList };
