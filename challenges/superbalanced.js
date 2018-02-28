ffd3e02669ce72cd74a3bcb7bd742eb1e5645009
Kunal Patel <kunalpatel73@gmail.com>
2018-02-28 10:08:41 -0800

    completed

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,40 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  let depthOfLeaves = [];
+  
+  if (!root) {
+    return true;
+  }
+  
+  let nodes = [];
+  nodes.push([root, 0]);
 
+  while (nodes.length) {
+    let nodePair = nodes.pop();
+    let node = nodePair[0]
+    depth = nodePair[1];
+    
+
+    if (!node.left && !node.right) {
+      
+      if (depths.indexOf(depth) < 0) {
+        depths.push(depth);
+        
+        if ((depths.length > 2) || (depths.length === 2 && Math.abs(depths[0]- depths[1]) > 1)) {
+          return false;
+        }
+      }
+    } else {
+      if (node.left) {
+        nodes.push(nodes.left, depth + 1);
+      }
+      if (nodes.right) {
+        nodes.push(nodes.right, depth + 1);
+      }
+    }
+  } 
+  return true;
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
