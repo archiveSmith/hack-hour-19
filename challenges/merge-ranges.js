d1abd23cd7fdd2a4c6afe9a266c330dcde4b2332
Justin Yip <justinyip@Justins-MacBook-Air.local>
2018-03-02 09:49:08 -0800

    mergeRanges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,15 @@
 
 
 function mergeRanges(array) {
+    var result = [], last;
 
+    array.forEach(function (r) {
+        if (!last || r[0] > last[1])
+            result.push(last = r);
+        else if (r[1] > last[1])
+            last[1] = r[1];
+    });
+
+    return result;
 }
-
 module.exports = mergeRanges;
