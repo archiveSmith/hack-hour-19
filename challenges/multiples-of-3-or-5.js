739a253c098f7e602390b892b903b2158aeba04e
Annie <annie.difiore@gmail.com>
2018-03-05 09:51:33 -0800

    streamline

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -29,24 +29,10 @@ function sumMultiples3Or5Below1000() {
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
 
-  for (let i=0; i<z; i+=x) {
-    sum += i;
-  }
-  for (let j=0; j<z; j+=y) {
-    sum += j;
-  }
-  // subtract duplicate
-  let lcm;
-  const smaller = Math.min(x,y);
-  for (let l=smaller; l<(x*y); l++){
-    if (l%x === 0 && l%y === 0) {
-      lcm = l;
-      break;
+  for (let i=0; i<z; i+=1) {
+    if (i%x === 0 || i%y === 0){
+      sum += i;
     }
-  }
-  
-  for (let k=0; k<z; k+=lcm) {
-    sum -= k;
   }
 
   return sum;
