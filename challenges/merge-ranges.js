062c125193af2bbd7863a2e5ee4c5ebd396fcb3d
Annie <annie.difiore@gmail.com>
2018-03-06 09:52:05 -0800

    merge ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -15,8 +15,13 @@ function mergeRanges(array) {
 
   let result = [array[0]];
   for (let i = 1; i < array.length; i++) {
-    
+    if (array[i][0] > result[result.length - 1][1]) {
+      result.push(array[i]);
+    } else {
+      result[result.length - 1][1] = array[i][1];
+    }
   }
+  return result;
 }
 
 module.exports = mergeRanges;
