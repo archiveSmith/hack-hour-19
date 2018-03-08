c00f985b5c4139ef79f63ad638159711b1109cb1
Mejin Leechor <amleechor@gmail.com>
2018-03-08 09:36:32 -0800

    Delete dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -13,7 +13,31 @@
 
 
 function deleteDups(head) {
-
+  if (!head) return head;
+  let i = head;
+  while (i && i.next) {
+    let j = i.next;
+    while (j) {
+      if (i.val === j.val) i.next = j.next;
+      j = j.next;
+    }
+    i = i.next;
+  }
+  return head;
 }
+
+function Node(value) {
+  this.val = value;
+  this.next = null;
+}
+
+/*
+const linkedList = new Node(1);
+linkedList.next = new Node(1);
+linkedList.next.next = new Node(2);
+linkedList.next.next.next = new Node(2);
+console.log(JSON.stringify(deleteDups(linkedList), null, 2));
+*/
+
 
 module.exports = deleteDups;
