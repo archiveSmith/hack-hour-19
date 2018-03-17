58b87a6eb397ce126ef82b7b396eaabac5d15ce5
Max White <maxwell.oliver.white@gmail.com>
2018-03-17 09:43:44 -0700

    bubble sort, mostly untested

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,26 @@
 
 
 function bubbleSort(array) {
-
+  if (!Array.isArray(array)) return;
+  for (let i = 0; i < array.length; i ++) {
+    let noChange = true;
+    for (let j = 0; j < array.length - 1 - i; j ++) {
+      if (array[j] > array[j+1]) {
+        noChange = false;
+        const temp = array[j+1];
+        array[j+1] = array[j];
+        array[j] = temp;
+      }
+    }
+    // if (noChange) break; // possible efficiency raiser
+  }
+  return array;
 }
+
+function tests() {
+  console.log(bubbleSort([3, 6, 334, 7, 6567, 5, -23]));
+}
+
+//tests();
 
 module.exports = bubbleSort;
