e390a383f062f3d351ca4d5b7607baa743e9603a
scottyb <scottyb@scottybs-MacBook.local>
2018-05-07 11:43:59 -0700

    modeMean updates

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -1,3 +1,4 @@
+function modemean(array) {
 /*
  * Given an array of numbers, determine if the mode and mean of the array are equivalent
  *
@@ -8,49 +9,46 @@
  * Return true or false
  *
  */
-<<<<<<< HEAD
-var newmyArr = array.sort().reverse();
+
+let testArr = [1, 2, 3, 2];
 
 function modemean(array) {
-  
-  var total = 0;
-  for(var i = 0; i<newmyArr.length; i++ ) {
-  total += newmyArr[i];
-  }
-  var myMean = total/newmyArr.length;
-  var newMean = Math.floor(myMean)
- 
+  return mean(array) === mode(array);
+}
 
+function mean(array) {
+  var sum = array.reduce(function(sum, el) {
+    return sum + el;
+  });
+  return Math.floor(sum / array.length);
+}
 
-var mode = 0;
-var count = 0;
-  for(var s = 0; s < newmyArr.length; s++){
-    for(var j = 0; j < s; j++){
-       if(newmyArr[j] === newmyArr[s]){
-           mode = newmyArr[j];
-           count++;
-           console.log(count);
-       }
+function mode(array) {
+  var count = {};
+  // if number is already in object ( already appeared in the given array) increment value, otherwise set it equal to one.
+  array.forEach(function(num) {
+    if (num in count) {
+      return count[num]++;
+    }
+    count[num] = 1;
+  });
+  // set max to -infinity so all numbers regardless of value will be included.
+  var max = -Infinity;
+  var modeCandidates;
+  for (var num in count) {
+    if (count[num] > max) {
+      // numbers with most occurences in array become get updated to mode candidate variable.
+      modeCandidates = [num];
+      max = count[num];
+      
+    } else if (count[num] === max) {
+      modeCandidates.push(num);
     }
   }
-  console.log("my mode " + mode)
-  console.log("my newMean " + newMean)
-  
-  
-  if(mode === newMean) {
-    return true;
-  }  else  {
-    return false;
-  }
-
+  // then we return largest of mode candidates
+  return Math.max.apply(this, modeCandidates);
 }
-modemean(array)
-=======
-
-
-function modemean(array) {
-
+modemean(testArr);
 }
->>>>>>> c060d19ffed168e7524c284bc3486ccd146b9a9b
 
 module.exports = modemean;
